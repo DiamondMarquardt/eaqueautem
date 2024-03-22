24bae58cf14b8bdf18dc940466698fecc9074424
DiamondMarquardt <mqcfvirsab@outlook.com>
2024-03-22 08:13:27 +0800

    qui

---
+++ packages/playground/src/components/vm_deployment_table.vue
@@ -0,0 +1,161 @@
+<template>
+  <div>
+    <v-alert v-if="!loading && count && items.length < count" type="warning" variant="tonal">
+      Failed to load <strong>{{ count - items.length }}</strong> deployment{{ count - items.length > 1 ? "s" : "" }};
+      <span>
+        This might happen because the node is down or it's not reachable or the deployment{{
+          count - items.length > 1 ? "s are" : " is"
+        }}
+        encrypted by another key.
+      </span>
+    </v-alert>
+
+    <ListTable
+      :headers="filteredHeaders"
+      :items="items"
+      :loading="loading"
+      :deleting="deleting"
+      :model-value="$props.modelValue"
+      @update:model-value="$emit('update:model-value', $event)"
+      :no-data-text="`No ${projectName} deployments found on this account.`"
+      @click:row="$attrs['onClick:row']"
+    >
+      <template #[`item.name`]="{ item }">
+        {{ item.value[0].name }}
+      </template>
+
+      <template #[`item.ipv4`]="{ item }">
+        {{ item.value[0].publicIP?.ip?.split("/")?.[0] || item.value[0].publicIP?.ip || "-" }}
+      </template>
+
+      <template #[`item.ipv6`]="{ item }">
+        {{ item.value[0].publicIP?.ip6 || "-" }}
+      </template>
+
+      <template #[`item.planetary`]="{ item }">
+        {{ item.value[0].planetary || "-" }}
+      </template>
+
+      <template #[`item.flist`]="{ item }">
+        <v-tooltip :text="item.value[0].flist" location="bottom right">
+          <template #activator="{ props }">
+            <p v-bind="props">
+              {{ item.value[0].flist.replace("https://hub.grid.tf/", "").replace(".flist", "") }}
+            </p>
+          </template>
+        </v-tooltip>
+      </template>
+
+      <template #[`item.billing`]="{ item }">
+        {{ item.value[0].billing }}
+      </template>
+      <template #[`item.actions`]="{ item }">
+        <v-chip color="error" variant="tonal" v-if="deleting && ($props.modelValue || []).includes(item.value)">
+          Deleting...
+        </v-chip>
+        <v-btn-group variant="tonal" v-else>
+          <slot :name="projectName + '-actions'" :item="item"></slot>
+        </v-btn-group>
+      </template>
+    </ListTable>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, onMounted, ref } from "vue";
+
+import { useProfileManager } from "../stores";
+import { getGrid, updateGrid } from "../utils/grid";
+import { loadVms, mergeLoadedDeployments } from "../utils/load_deployment";
+
+const profileManager = useProfileManager();
+
+const props = defineProps<{
+  projectName: string;
+  modelValue: any[];
+  deleting: boolean;
+}>();
+defineEmits<{ (event: "update:model-value", value: any[]): void }>();
+
+const loading = ref(false);
+const count = ref<number>();
+const items = ref<any[]>([]);
+
+onMounted(loadDeployments);
+async function loadDeployments() {
+  items.value = [];
+  loading.value = true;
+  const grid = await getGrid(profileManager.profile!, props.projectName);
+  const chunk1 = await loadVms(grid!);
+  const chunk2 = await loadVms(updateGrid(grid!, { projectName: props.projectName.toLowerCase() }));
+  const filter =
+    props.projectName === ProjectName.VM
+      ? undefined
+      : ([vm]: [{ flist: string }]) => vm.flist.replace(/-/g, "").includes(props.projectName.toLowerCase());
+
+  const chunk3 =
+    props.projectName === ProjectName.Fullvm
+      ? { count: 0, items: [] }
+      : await loadVms(updateGrid(grid!, { projectName: "" }), { filter });
+  const vms = mergeLoadedDeployments(chunk1, chunk2, chunk3 as any);
+
+  count.value = vms.count;
+  items.value = vms.items;
+
+  loading.value = false;
+}
+
+const filteredHeaders = computed(() => {
+  let headers = [
+    { title: "PLACEHOLDER", key: "data-table-select" },
+    { title: "Name", key: "name" },
+    { title: "Public IPv4", key: "ipv4" },
+    { title: "Public IPv6", key: "ipv6" },
+    { title: "Planetary Network IP", key: "planetary" },
+    { title: "Flist", key: "flist" },
+    { title: "Cost", key: "billing" },
+    { title: "Actions", key: "actions" },
+  ];
+
+  const IPV6Solutions = [ProjectName.VM, ProjectName.Fullvm] as string[];
+
+  const IPV4Solutions = [
+    ProjectName.VM,
+    ProjectName.Fullvm,
+    ProjectName.Presearch,
+    ProjectName.Algorand,
+    ProjectName.Subsquid,
+    ProjectName.Umbrel,
+  ] as string[];
+
+  const flistSolutions = [ProjectName.VM, ProjectName.Fullvm] as string[];
+
+  if (!IPV6Solutions.includes(props.projectName)) {
+    headers = headers.filter(h => h.key !== "ipv6");
+  }
+
+  if (!IPV4Solutions.includes(props.projectName)) {
+    headers = headers.filter(h => h.key !== "ipv4");
+  }
+
+  if (!flistSolutions.includes(props.projectName)) {
+    headers = headers.filter(h => h.key !== "flist");
+  }
+
+  return headers;
+});
+
+defineExpose({ loadDeployments });
+</script>
+
+<script lang="ts">
+import { ProjectName } from "../types";
+import ListTable from "./list_table.vue";
+
+export default {
+  name: "VmDeploymentTable",
+  components: {
+    ListTable,
+  },
+};
+</script>
---
+++ packages/playground/src/utils/load_deployment.ts
@@ -0,0 +1,134 @@
+import type { GridClient } from "@threefold/grid_client";
+
+import { formatConsumption } from "./contracts";
+import { updateGrid } from "./grid";
+import { normalizeError } from "./helpers";
+
+export interface LoadedDeployments<T> {
+  count: number;
+  items: T[];
+}
+
+export interface LoadVMsOptions {
+  filter?(vm: any): boolean;
+}
+export async function loadVms(grid: GridClient, options: LoadVMsOptions = {}) {
+  const machines = await grid.machines.list();
+  let count = machines.length;
+
+  const promises = machines.map(name => {
+    return grid.machines.getObj(name).catch(e => {
+      console.log(
+        `%c[Error] failed to load deployment with name ${name}:\n${normalizeError(e, "No errors were provided.")}`,
+        "color: rgb(207, 102, 121)",
+      );
+      return null;
+    });
+  });
+  const items = await Promise.all(promises);
+  const vms = items
+    .map((item: any, index) => {
+      if (item) {
+        item.deploymentName = machines[index];
+        item.projectName = grid.clientOptions!.projectName;
+        item.forEach((i: any) => {
+          i.deploymentName = machines[index];
+          i.projectName = grid.clientOptions!.projectName;
+        });
+      }
+      return item;
+    })
+    .filter(item => item && item.length > 0)
+    .filter(item => {
+      if (options.filter && !options.filter(item)) {
+        count--;
+        return false;
+      }
+      return true;
+    }) as any[][];
+  const consumptions = await Promise.all(
+    vms.map(vm => {
+      return grid.contracts.getConsumption({ id: vm[0].contractId }).catch(() => undefined);
+    }),
+  );
+  const wireguards = await Promise.all(
+    vms.map(vm => getWireguardConfig(grid, vm[0].interfaces[0].network).catch(() => [])),
+  );
+
+  const data = vms.map((vm, index) => {
+    vm[0].billing = formatConsumption(consumptions[index] as number);
+    if (wireguards[index] && wireguards[index].length > 0) {
+      vm[0].wireguard = wireguards[index][0];
+    }
+
+    return vm;
+  });
+
+  return <LoadedDeployments<any[]>>{
+    count,
+    items: data,
+  };
+}
+export function getWireguardConfig(grid: GridClient, name: string) {
+  const projectName = grid.clientOptions!.projectName;
+  return updateGrid(grid, { projectName: "" })
+    .networks.getWireGuardConfigs({ name })
+    .finally(() => updateGrid(grid, { projectName }));
+}
+
+export type K8S = { masters: any[]; workers: any[]; deploymentName: string; wireguard?: any };
+export async function loadK8s(grid: GridClient) {
+  const clusters = await grid.k8s.list();
+  const promises = clusters.map(name => {
+    return grid.k8s.getObj(name).catch(e => {
+      console.log(
+        `%c[Error] failed to load deployment with name ${name}:\n${normalizeError(e, "No errors were provided.")}`,
+        "color: rgb(207, 102, 121)",
+      );
+      return null;
+    });
+  });
+  const items = (await Promise.all(promises)) as any[];
+  const k8s = items
+    .map((item, index) => {
+      if (item) {
+        item.deploymentName = clusters[index];
+        item.projectName = grid.clientOptions!.projectName;
+      }
+      return item;
+    })
+    .filter(item => item && item.masters.length > 0) as K8S[];
+  const consumptions = await Promise.all(
+    k8s.map(cluster => {
+      return grid.contracts.getConsumption({ id: cluster.masters[0].contractId }).catch(() => undefined);
+    }),
+  );
+
+  const wireguards = await Promise.all(
+    k8s.map(cluster => getWireguardConfig(grid, cluster.masters[0].interfaces[0].network).catch(() => [])),
+  );
+  const data = k8s.map((cluster, index) => {
+    cluster.masters[0].billing = formatConsumption(consumptions[index] as number);
+
+    if (wireguards && wireguards[index]) {
+      cluster.wireguard = wireguards[index][0];
+    }
+    return cluster as K8S;
+  });
+
+  return <LoadedDeployments<K8S>>{
+    count: clusters.length,
+    items: data,
+  };
+}
+
+export function mergeLoadedDeployments<T>(...deployments: LoadedDeployments<T>[]) {
+  return deployments.reduce(
+    (res, current) => {
+      res.count += current.count;
+      res.items = res.items.concat(current.items);
+      return res;
+    },
+    { count: 0, items: [] },
+  );
+}
